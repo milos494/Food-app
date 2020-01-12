64fad0ae2bfcf042386766407160ae273ba69f64
Milos Loncarevic <m.loncarevic@mloncarevic-n.levi9.com>
2020-01-12 21:34:57 +0100

    feat(server): constraints

--- food_app.sql
+++ food_app.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1deb2
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 22, 2019 at 06:20 PM
--- Server version: 8.0.18
--- PHP Version: 7.3.11-0ubuntu0.19.10.1
+-- Generation Time: Jan 12, 2020 at 09:32 PM
+-- Server version: 5.7.28-0ubuntu0.18.04.4
+-- PHP Version: 7.2.24-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,24 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `food_app`
+-- Database: `FoodApp`
 --
-CREATE DATABASE IF NOT EXISTS `food_app` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `food_app`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `average_rating`
---
-
-DROP TABLE IF EXISTS `average_rating`;
-CREATE TABLE IF NOT EXISTS `average_rating` (
-  `recipe_id` int(11) NOT NULL,
-  `nmber_of_voters` int(11) NOT NULL,
-  `average_rating` double NOT NULL,
-  PRIMARY KEY (`recipe_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS `FoodApp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `FoodApp`;
 
 -- --------------------------------------------------------
 
@@ -48,10 +32,21 @@ DROP TABLE IF EXISTS `ingredients`;
 CREATE TABLE IF NOT EXISTS `ingredients` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `unit_of_measure` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `image` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `unitOfMeasure` varchar(30) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `validatedByAdmin` tinyint(4) NOT NULL DEFAULT '0',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `userId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_a955029b22ff66ae9fef2e161f` (`name`),
+  KEY `FK_9a8a13cc60b4a4a067679cde290` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONS FOR TABLE `ingredients`:
+--   `userId`
+--       `user` -> `id`
+--
 
 -- --------------------------------------------------------
 
@@ -61,12 +56,22 @@ CREATE TABLE IF NOT EXISTS `ingredients` (
 
 DROP TABLE IF EXISTS `ratings`;
 CREATE TABLE IF NOT EXISTS `ratings` (
-  `recipe_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
   `rating` int(11) NOT NULL,
-  KEY `recipe_id` (`recipe_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `recipeId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_1a12b19c1c1a05b203ef6314e7` (`userId`,`recipeId`),
+  KEY `FK_bbfab9d204f8a93f7da1ab97b32` (`recipeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONS FOR TABLE `ratings`:
+--   `userId`
+--       `user` -> `id`
+--   `recipeId`
+--       `recipes` -> `id`
+--
 
 -- --------------------------------------------------------
 
@@ -78,27 +83,46 @@ DROP TABLE IF EXISTS `recipes`;
 CREATE TABLE IF NOT EXISTS `recipes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `user_id` int(11) NOT NULL,
   `preparation` varchar(10000) NOT NULL,
-  `image` int(11) DEFAULT NULL,
+  `image` varchar(100) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `description` varchar(10000) NOT NULL,
+  `preparationTime` int(11) NOT NULL DEFAULT '100',
+  `numberOfPersons` int(11) NOT NULL DEFAULT '4',
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `FK_ad4f881e4b9769d16c0ed2bb3f0` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONS FOR TABLE `recipes`:
+--   `userId`
+--       `user` -> `id`
+--
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `recipes_ingredients`
+-- Table structure for table `recipe_ingredients`
 --
 
-DROP TABLE IF EXISTS `recipes_ingredients`;
-CREATE TABLE IF NOT EXISTS `recipes_ingredients` (
-  `recipe_id` int(11) NOT NULL,
-  `ingredient_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `recipe_ingredients`;
+CREATE TABLE IF NOT EXISTS `recipe_ingredients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `amount` int(11) NOT NULL,
-  KEY `recipe_id` (`recipe_id`),
-  KEY `ingredient_id` (`ingredient_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `recipeId` int(11) DEFAULT NULL,
+  `ingredientId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_77d03a398e85d333b09b22cda9` (`recipeId`,`ingredientId`),
+  KEY `FK_05a2b62604dfd9840f4cda76a93` (`ingredientId`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONS FOR TABLE `recipe_ingredients`:
+--   `ingredientId`
+--       `ingredients` -> `id`
+--   `recipeId`
+--       `recipes` -> `id`
+--
 
 -- --------------------------------------------------------
 
@@ -109,49 +133,51 @@ CREATE TABLE IF NOT EXISTS `recipes_ingredients` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `e-mail` varchar(255) NOT NULL,
-  `password` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `date_of_birth` date NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `user_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` varchar(5) NOT NULL DEFAULT 'other',
+  `token` varchar(255) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `e-mail` (`e-mail`),
-  UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `IDX_e12875dfb3b1d92d7d7c5377e2` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONS FOR TABLE `user`:
+--
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `average_rating`
+-- Constraints for table `ingredients`
 --
-ALTER TABLE `average_rating`
-  ADD CONSTRAINT `average_rating_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `ingredients`
+  ADD CONSTRAINT `FK_9a8a13cc60b4a4a067679cde290` FOREIGN KEY (`userId`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `ratings`
 --
 ALTER TABLE `ratings`
-  ADD CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `ratings_ibfk_2` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_4d0b0e3a4c4af854d225154ba40` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_bbfab9d204f8a93f7da1ab97b32` FOREIGN KEY (`recipeId`) REFERENCES `recipes` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `recipes`
 --
 ALTER TABLE `recipes`
-  ADD CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `FK_ad4f881e4b9769d16c0ed2bb3f0` FOREIGN KEY (`userId`) REFERENCES `user` (`id`);
 
 --
--- Constraints for table `recipes_ingredients`
+-- Constraints for table `recipe_ingredients`
 --
-ALTER TABLE `recipes_ingredients`
-  ADD CONSTRAINT `recipes_ingredients_ibfk_1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `recipes_ingredients_ibfk_2` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-COMMIT;
+ALTER TABLE `recipe_ingredients`
+  ADD CONSTRAINT `FK_05a2b62604dfd9840f4cda76a93` FOREIGN KEY (`ingredientId`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_2d7f407ae694e91bb3da1798c61` FOREIGN KEY (`recipeId`) REFERENCES `recipes` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
